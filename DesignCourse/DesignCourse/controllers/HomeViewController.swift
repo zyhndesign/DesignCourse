0649de52349bca0bba6ec3c62cd99414b6eb973a
zyhndesign <zyhndesign@zyhnmatoMacBook-Air.local>
2015-09-28 17:51:51 +0800

    add menu animation

--- DesignCourse/DesignCourse/controllers/HomeViewController.swift
+++ DesignCourse/DesignCourse/controllers/HomeViewController.swift
@@ -13,8 +13,11 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var menuTableView: UITableView!
     
-    
+    @IBOutlet weak var menuPanel: UIView!
+    @IBOutlet weak var contentPanel: UIView!
     @IBOutlet weak var contentScrollView: UIScrollView!
+    
+    var menuOpenFlag:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,4 +30,61 @@ class HomeViewController: UIViewController {
     }
     
     
+    @IBAction func menuBtnClick(sender: AnyObject) {
+        
+        //self.menuAnimation(contentPanel);
+        self.menuSpringAnimation(contentPanel)
+    }
+    
+    func menuAnimation(view:UIView){
+        
+        let anim:POPBasicAnimation = POPBasicAnimation.init(propertyNamed: kPOPLayerPosition)
+        view.pop_removeAllAnimations();
+        anim.fromValue = NSValue(CGPoint:CGPointMake(view.center.x, view.center.y))
+        if (menuOpenFlag){
+            anim.toValue =  NSValue(CGPoint:CGPointMake(view.center.x - 220, view.center.y))
+        }
+        else{
+            anim.toValue =  NSValue(CGPoint:CGPointMake(view.center.x + 220, view.center.y))
+        }
+        
+        anim.completionBlock = {(animation, finished) in
+            if (finished){
+                if (self.menuOpenFlag){
+                    self.menuOpenFlag = false
+                }
+                else{
+                    self.menuOpenFlag = true
+                }
+            }
+        }
+        contentPanel.layer.pop_addAnimation(anim, forKey: "Animation")
+    }
+    
+    func menuSpringAnimation(view:UIView){
+        
+        view.pop_removeAllAnimations();
+        let anim:POPSpringAnimation = POPSpringAnimation.init(propertyNamed:kPOPLayerPosition)
+        anim.springBounciness = 10
+        anim.springSpeed = 20
+        
+        if (menuOpenFlag){
+            anim.toValue =  NSValue(CGPoint:CGPointMake(view.center.x - 220, view.center.y))
+        }
+        else{
+            anim.toValue =  NSValue(CGPoint:CGPointMake(view.center.x + 220, view.center.y))
+        }
+        
+        anim.completionBlock = {(animation, finished) in
+            if (finished){
+                if (self.menuOpenFlag){
+                    self.menuOpenFlag = false
+                }
+                else{
+                    self.menuOpenFlag = true
+                }
+            }
+        }
+        contentPanel.layer.pop_addAnimation(anim, forKey: "Animation")
+    }
 }
